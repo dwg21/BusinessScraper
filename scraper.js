a40c8277d64b43c1ce6b84caee951f43f12a4fe6
dwg21 <95627833+dwg21@users.noreply.github.com>
2023-07-20 18:35:55 +0100

    code cleaned

--- scraper.js
+++ scraper.js
@@ -19,35 +19,6 @@ async function fetchCompanyWebsite(url) {
     return htmlContent;
 }
 
-// Function to address detail from text
-// function extractAddressInfo(text) {
-//   // Regular expression for UK postcodes
-//     const ukPostcodeRegex = /\b(?:[A-Z]{1,2}[0-9R][0-9A-Z]? ?[0-9][ABD-HJLNP-UW-Z]{2})\b/gi;
-//     let extractedPostcodes = text.match(ukPostcodeRegex) || [];
-//     extractedPostcodes = extractedPostcodes.filter((value, index, self) => self.indexOf(value) === index);
-    
-
-
-//   // Regex expression for the lines of addresses
-//     const addressRegex = /\b\d+\s+\w+(?:\s+\w+)*\s+(?:street|road|avenue|lane|drive|court|house)\b/gi;
-//     let extractedStreetNames = text.match(addressRegex) || [];
-
-//     // Filter out potential postcode from the addresses
-//     extractedStreetNames = extractedStreetNames.filter((address) => !ukPostcodeRegex.test(address));
-
-//     // Clean up addresses (remove extra whitespace and newlines)
-//     extractedStreetNames = extractedStreetNames.map((address) => address.replace(/\s+/g, ' ').trim());
-
-//     //Remove duplicates
-//     extractedStreetNames = extractedStreetNames.filter((value, index, self) => self.indexOf(value) === index);
-
-
-
-
-//     return {
-//     extractedPostcodes, extractedStreetNames,
-//     };
-// }
 
 // Function to scrape data from the HTML content
 function scrapeDataFromHTML(html) {
@@ -62,25 +33,13 @@ function scrapeDataFromHTML(html) {
     let extractedEmails = knwlInstance.get('emails').map((email) => email.address);
     extractedEmails = extractedEmails.filter((value, index, self) => self.indexOf(value) === index);
     
+    // Extract numbers using custom plugin
     knwlInstance.register('uknumbers', UKPhoneNumbersPlugin);
     const extractedPhoneNumbers = knwlInstance.get('uknumbers').map((number) => number.phone);
 
-
-
-  // Extract addresses using the previously defined function
-    // const extractedAddresses = extractAddressInfo(extractedText);
+    // Extract addresses using custom plugin
     knwlInstance.register('ukAddresses', UKAddresses);
     const extractedAddresses = knwlInstance.get('ukAddresses').map((address) => address.address);
-
-//   // Regular expression to match phone numbers in various formats
-//     const phoneRegex = /(?:(?:\+?\d{1,3}[-.\s()]*?)?\d{3,}[-.\s()]*\d{3,}[-.\s()]*\d{1,9}\b)(?![.\d])/g;
-
-//   // Extract phone numbers using regex
-//     const rawPhones = extractedText.match(phoneRegex) || [];
-//     const extractedPhoneNumbers = rawPhones
-//     .map((phone) => phone.replace(/\D/g, '')) // Remove non-digit characters
-//     .filter((phone, index, self) => self.indexOf(phone) === index) // Filter out duplicates
-//     .filter((phone) => phone.length >= 11 && phone.length <= 12); // Filter out numbers with less than 11 or more than 12 digits
 
 
     return { extractedEmails, extractedPhoneNumbers, extractedAddresses };
@@ -107,9 +66,9 @@ async function scrapeCompanyInfo(email) {
 
 
 //Change variable here to test different emails
-scrapeCompanyInfo('hello@mrwardrobe.co.uk');
+//scrapeCompanyInfo('hello@mrwardrobe.co.uk');
 scrapeCompanyInfo(CanddiEmail)
-scrapeCompanyInfo('hello@mh-partnership.co.uk');
+//scrapeCompanyInfo('hello@mh-partnership.co.uk');
 // scrapeCompanyInfo('20info@pmwcom.co.uk')
 
 
